965eb87bc93962e27d1422e868ac7c6a6385da76
Camilo <camilo.svs@gmail.com>
2021-10-17 13:12:19 -0300

    CRUD de modulos

--- Back-End/controllers/modulos.js
+++ Back-End/controllers/modulos.js
@@ -74,12 +74,71 @@ const actualizarModulo = async(req, res = response) => {
       modulo:moduloUpdate
     })
   } catch (error) {
-    
+    console.log(error);
+    res.status(500).json({
+        ok: false,
+        msg: 'Error en bd actualizar modulo'
+    });
   }
   
 }
 
+const getModulos = async(req, res = response) => {
+  try {
+    const modulos = await Modulo.find();
+    if(!modulos || modulos.length === 0){
+      return res.status(404).json({
+        ok:false,
+        msg:"No existen modulos"
+      })
+    }
+    res.json({
+      ok:true,
+      msg:"Modulos encontrados",
+      modulos
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok:false,
+      msg:"error en bd, obtener modulos"
+    })
+  }
+}
+
+const eliminarModulo = async(req, res = response) => {
+  
+  const moduloId = req.params.id;
+
+  try {
+    const modulo = await Modulo.findById(moduloId);
+
+    if (!modulo) {
+
+      res.status(400).json({
+        ok:false,
+        msg:'El modulo no existe'
+      })
+      
+    }
+
+    await Modulo.findByIdAndDelete(moduloId);
+
+    res.json({
+      ok:true,
+      msg:'Modulo eliminado correctamente'
+    })
+  } catch (error) {
+    
+  }
+ 
+
+}
+
+
 module.exports = {
   crearModulo,
-  actualizarModulo
+  actualizarModulo,
+  getModulos,
+  eliminarModulo
 }
